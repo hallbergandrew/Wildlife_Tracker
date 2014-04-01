a9326cfd357fca3e1fc82816e9cb06bb8faf7c19
Andrew Hallberg & Kimberlee Redman <student@epicodus.com>
2014-04-01 10:32:36 -0700

    initialized and some functionality for adding species

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WildlifeTracker::Application.config.secret_key_base = '1bf001956d2edda39b3b680d3a1c64a5fff254ee60e327ab1a8ecf3b0acb38af8570982033120d0cffb10e37be7d0ca818e9ef9db5b1f212f89e58ef6f6fd442'
